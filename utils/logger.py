6a67a3d05876b20190933606098d2b283b42244e
Luis Egui <misionpage@gmail.com>
2025-08-30 14:18:37 +0200

    refactor(logger): simplify logging format by removing function name and line number

--- utils/logger.py
+++ utils/logger.py
@@ -13,7 +13,7 @@ def configure_logging(debug_mode: bool) -> None:
     log_level = logging.DEBUG if debug_mode else logging.INFO
     logging.basicConfig(
         level=log_level,
-        format="%(asctime)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s",
+        format="%(asctime)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         encoding="utf-8",
     )
