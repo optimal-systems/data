8396dba5698368bec9881dabecc1a29fd672166b
Luis Egui <misionpage@gmail.com>
2025-08-30 14:18:55 +0200

    feat(extract): implement supermarket data extraction and transformation

--- ahorramas/extract.py
+++ ahorramas/extract.py
@@ -3,6 +3,7 @@ import time
 import logging
 from os.path import dirname, abspath
 import requests
+import polars as pl
 
 # Add the parent directory to the sys.path
 sys.path.append(dirname(dirname(abspath(__file__))))
@@ -25,8 +26,10 @@ def fetch_supermarkets():
 
     for _ in range(0, retries):
         try:
+            logging.info("Fetching supermarkets data from Ahorramas API")
             response = session.get(url, headers={}, params=params, timeout=timeout)
             session.close()
+            logging.info("Supermarkets data fetched from Ahorramas API")
             return response.json()
 
         except requests.Timeout as exception:
@@ -41,3 +44,33 @@ def fetch_supermarkets():
         "Max retries reached. Failed to fetch supermarkets data from Ahorramas API"
     )
     raise RuntimeError("Failed to fetch supermarkets from Ahorramas API")
+
+
+def extract_supermarkets() -> pl.DataFrame:
+    """
+    Transform the supermarkets data from Ahorramas API into a polars DataFrame
+
+    Parameters:
+        df: pl.DataFrame: The supermarkets data from Ahorramas API
+
+    Returns:
+        pl.DataFrame: The transformed supermarkets data
+    """
+    raw_data = fetch_supermarkets()
+    raw_supermarkets = raw_data["stores"]
+
+    items = []
+
+    for supermarket in raw_supermarkets:
+        items.append(
+            {
+                "store_id": supermarket["codtda"],
+                "address": supermarket["direccion"],
+                "schedule": supermarket["horario"],
+                "holidays": supermarket["festivos"],
+                "latitude": supermarket["latitude"],
+                "longitude": supermarket["longitude"],
+            }
+        )
+
+    return pl.DataFrame(items)
