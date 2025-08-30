42f084003d7e7551fc95e7889ba1cbccb6f7e9e4
Luis Egui <misionpage@gmail.com>
2025-08-30 02:41:36 +0200

    feat(cli): implement fetch_supermarkets command to retrieve data from Ahorramas API

---
+++ ahorramas/extract.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import time
+import logging
+import json
+from os.path import dirname, abspath
+import requests
+
+# Add the parent directory to the sys.path
+sys.path.append(dirname(dirname(abspath(__file__))))
+
+
+def fetch_supermarkets():
+    """
+    Fetch supermarkets from Ahorramas API
+    """
+    url = "https://www.ahorramas.com/on/demandware.store/Sites-Ahorramas-Site/es/Stores-FindStore"
+    params = {
+        "update": "true",
+        "showMap": "false",
+        "postalCodeInput": "España",
+    }
+    session = requests.Session()
+    retries = 5
+    timeout = 5
+    delay = 5
+
+    for _ in range(0, retries):
+        try:
+            response = session.get(url, headers={}, params=params, timeout=timeout)
+            session.close()
+            return response.json()
+
+        except requests.Timeout as exception:
+            logging.error("Timeout error: %s", exception)
+            time.sleep(delay)
+
+        except requests.RequestException as exception:
+            logging.warning("Request error: %s", exception)
+            time.sleep(delay)
+
+    logging.error(
+        "Max retries reached. Failed to fetch supermarkets data from Ahorramas API"
+    )
+    raise RuntimeError("Failed to fetch supermarkets from Ahorramas API")
--- ahorramas/main.py
+++ ahorramas/main.py
@@ -1,16 +1,27 @@
 import click
 
+import sys
+from os.path import abspath, dirname
+
+# Add the parent directory to the sys.path
+sys.path.append(dirname(dirname(abspath(__file__))))
+
+from utils.logger import configure_logging
+
 
 @click.group()
+@click.option("--debug", is_flag=True, help="Enable debug mode.")
 @click.pass_context
-def cli(ctx):
+def cli(ctx, debug):
     """Ahorramas CLI Tool"""
     ctx.ensure_object(dict)
+    configure_logging(debug)
 
 
 @cli.command()
-def test():
-    print("Test")
+@click.pass_context
+def get_supermarkets(ctx):
+    pass
 
 
 if __name__ == "__main__":
